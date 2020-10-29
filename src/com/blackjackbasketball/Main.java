ead1edc1b5dab934ef176929fa33491e7b0ca05e
Student <student@DC-TECH-17.local>
2020-10-29 14:13:30 -0500

    Added Wildcard function and code for point system

--- src/com/blackjackbasketball/Main.java
+++ src/com/blackjackbasketball/Main.java
@@ -3,6 +3,8 @@ package com.blackjackbasketball;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	PointSystem points = new PointSystem();
+
+	points.wildCard();
     }
 }
--- src/com/blackjackbasketball/PointSystem.java
+++ src/com/blackjackbasketball/PointSystem.java
@@ -1,2 +1,77 @@
-package com.blackjackbasketball;public class PointSystem {
+package com.blackjackbasketball;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Random;
+
+public class PointSystem {
+
+    private int totalPoints;
+    private int roundNumber;
+    private String wildCardStatement;
+    Random r = new Random();
+
+
+    public int getTotalPoints() {
+        return totalPoints;
+    }
+
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+
+    public void setRoundNumber(int roundNumber) {
+        this.roundNumber = roundNumber;
+    }
+
+    public int addPoints(int value) {
+        totalPoints += value;
+
+        return totalPoints;
+    }
+
+    public int wildCard() {
+
+        int points = r.nextInt(totalPoints + 1);
+
+        //randomize - or + points
+        char[] list = {'-', '+'};
+        char randomOperand = list[r.nextInt(list.length)];
+
+        //System.out.println(randomOperand);
+        //System.out.println(list.length);
+
+
+        switch (randomOperand) {
+
+            case '-':
+                System.out.println("Muahahaha -" + points);
+                totalPoints = totalPoints - points;
+                break;
+
+            case '+':
+                System.out.println("Muahahaha +" + points);
+                totalPoints = totalPoints + points;
+                break;
+        }
+
+        System.out.println("You are now at " + totalPoints + " points.");
+        return totalPoints;
+    }
+
+    public String newRound() {
+
+        //Print Game intro
+        //Choices here
+        //Person makes choice
+        //Choice points identified
+        //generate new points
+        //return points total and list round number
+        //generate wildcard at every 4 rounds
+        //return wildcard results
+        //reprint options to play game
+
+
+        return "What's your next choice?";
+    }
 }
